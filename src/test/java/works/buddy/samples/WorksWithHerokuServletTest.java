eabf99b289b663318582a3ac876b941c2d410a79
bhagyashreep032 <patilbhagya094@gmail.com>
2025-03-15 02:33:12 +0530

    Update WorksWithHerokuServletTest.java

--- src/test/java/works/buddy/samples/WorksWithHerokuServletTest.java
+++ src/test/java/works/buddy/samples/WorksWithHerokuServletTest.java
@@ -36,6 +36,6 @@ public class WorksWithHerokuServletTest {
         when(response.getWriter()).thenReturn(writer);
 
         servlet.doGet(request, response);
-        assertEquals("WEB APP WORKING", new String( out.toByteArray(), "UTF-8"));
+        assertEquals("Hello\nWelcome to Tomcat server\nThis app is deployed by Bhagyashree!\nThank You!\n", new String(out.toByteArray(), "UTF-8"));
     }
 }
